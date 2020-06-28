80f48ce0521c7c910b4be3bec4cc9c41d09bc39b
AMARENDRA SAHOO <SS004700@sci.com>
2020-06-28 12:06:11 +0530

    changes from office desktop

---
+++ src/app/services/usbaccess.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { UsbRequest } from '../model/usb-request';
+import { PostResponse } from '../model/post-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsbaccessService {
+  // private url = 'http://nodefilemanagement-env.eba-md56muk4.ap-south-1.elasticbeanstalk.com/assets/usb/create';
+  private url = 'http://10.20.33.79:3000/assets/usb/create';
+
+  constructor(private http: HttpClient) { }
+
+  postUsbAccessReq(usbreqobj: UsbRequest): Observable<PostResponse>  {
+    console.log("inside service : ", usbreqobj)
+    return this.http.post<PostResponse>(this.url, usbreqobj);
+  } }
